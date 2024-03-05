08f783a04ff13feb4480939a9d05edfa698060ef
Michael Lucas <michael@samwise.nz>
2024-03-06 09:20:48 +1300

    Added schema registry to consumer

--- demo-event-hubs-consumer/Program.cs
+++ demo-event-hubs-consumer/Program.cs
@@ -2,8 +2,11 @@
 using Azure.Messaging.EventHubs.Consumer;
 using Azure.Messaging.EventHubs.Processor;
 using Azure.Storage.Blobs;
-using System.Text;
+using Azure.Data.SchemaRegistry;
+using Azure.Identity;
+using Microsoft.Azure.Data.SchemaRegistry.ApacheAvro;
 using Microsoft.Extensions.Configuration;
+using models;
 
 var builder = new ConfigurationBuilder()
     .AddUserSecrets<Program>();
@@ -35,20 +38,38 @@ await Task.Delay(TimeSpan.FromSeconds(30));
 // Stop the processing
 await processor.StopProcessingAsync();
 
-Task ProcessEventHandler(ProcessEventArgs eventArgs)
+async Task ProcessEventHandler(ProcessEventArgs eventArgs)
 {
-    // Write the body of the event to the console window
-    Console.WriteLine("\tReceived event: {0}", Encoding.UTF8.GetString(eventArgs.Data.Body.ToArray()));
-    Console.ReadLine();
-    return Task.CompletedTask;
+    try
+    {
+        // Create a schema registry client that you can use to serialize and validate data.  
+        var schemaRegistryClient =
+            new SchemaRegistryClient(configuration["SchemaRegistryEndpoint"], new DefaultAzureCredential());
+
+        // Create an Avro object serializer using the Schema Registry client object. 
+        var serializer = new SchemaRegistryAvroSerializer(schemaRegistryClient, configuration["SchemaGroupName"],
+            new SchemaRegistryAvroSerializerOptions { AutoRegisterSchemas = true });
+
+        // Deserialized data in the received event using the schema 
+        var vehicle = (Vehicle)await serializer.DeserializeAsync(eventArgs.Data, typeof(Vehicle));
+
+        // Print the received event
+        Console.WriteLine(
+            $"Received Vehicle: {vehicle.Id} -- {vehicle.Name} -- {vehicle.Description} -- {vehicle.Colour}");
+
+        await eventArgs.UpdateCheckpointAsync(eventArgs.CancellationToken);
+    }
+    catch (Exception e)
+    {
+        Console.WriteLine($"Exception occurred when trying to process a message: {e.Message}");
+    }
 }
 
-Task ProcessErrorHandler(ProcessErrorEventArgs eventArgs)
+static Task ProcessErrorHandler(ProcessErrorEventArgs eventArgs)
 {
     // Write details about the error to the console window
     Console.WriteLine(
         $"\tPartition '{eventArgs.PartitionId}': an unhandled exception was encountered. This was not expected to happen.");
     Console.WriteLine(eventArgs.Exception.Message);
-    Console.ReadLine();
     return Task.CompletedTask;
 }
